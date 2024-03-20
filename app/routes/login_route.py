653511418f73c7a4750387219ae32c868801cdf6
phanitallapudi <phanitallapudi@gmail.com>
2024-03-20 15:29:32 +0530

    add: added test cases for login route

--- app/routes/login_route.py
+++ app/routes/login_route.py
@@ -29,7 +29,7 @@ def login(request: OAuth2PasswordRequestForm = Depends()):
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'No user found with this {request.username} username')
     if not Hash.verify(user["password"], request.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Wrong Username or password')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'Wrong Username or password')
     
     # Get the user's role
     user_role = user["role"]
