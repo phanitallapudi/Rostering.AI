3e70895c727cfba4af228fcc77d7b0a4a702c902
phanitallapudi <phanitallapudi@gmail.com>
2024-03-20 19:27:13 +0530

    update: updated the entry in db for create_profile to include the uid

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -1,2 +1,6 @@
+import random
+
+def generate_unique_id():
+    return random.randint(100000, 999999)
 
 technicial_skill_set = ["router setup", "cable repair", "software troubleshooting", "fiber optics", "customer service"]
